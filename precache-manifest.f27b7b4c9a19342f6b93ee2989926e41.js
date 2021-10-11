7a32dbb9b039210e32a80ec31247b67635dab613
shamsherkc1993 <shamsher.kc.c4@gmail.com>
2021-10-11 19:12:04 +0900

    Updates

---
+++ precache-manifest.f27b7b4c9a19342f6b93ee2989926e41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853f0b5646a81a0627ce6f671d598088",
+    "url": "/first-react-project/index.html"
+  },
+  {
+    "revision": "deadbed560b864f91406",
+    "url": "/first-react-project/static/css/2.e4d55abd.chunk.css"
+  },
+  {
+    "revision": "ae2b07fbd5fa7297861e",
+    "url": "/first-react-project/static/css/main.94666672.chunk.css"
+  },
+  {
+    "revision": "deadbed560b864f91406",
+    "url": "/first-react-project/static/js/2.4bcccebe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/first-react-project/static/js/2.4bcccebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2b07fbd5fa7297861e",
+    "url": "/first-react-project/static/js/main.ec85d3b3.chunk.js"
+  },
+  {
+    "revision": "237d085a2ab162606f1b",
+    "url": "/first-react-project/static/js/runtime-main.5a430297.js"
+  }
+]);
